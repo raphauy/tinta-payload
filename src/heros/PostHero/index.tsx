39e382576fbec4237be8aad9744a463528cf1532
raphauy <rapha.uy@rapha.uy>
2024-12-16 08:27:39 -0300

    laguage on pot

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -46,7 +46,7 @@ export const PostHero: React.FC<{
             {hasAuthors && (
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-1">
-                  <p className="text-sm">Author</p>
+                  <p className="text-sm">{post.language === 'es' ? 'Autor' : 'Author'}</p>
 
                   <p>{formatAuthors(populatedAuthors)}</p>
                 </div>
@@ -54,7 +54,9 @@ export const PostHero: React.FC<{
             )}
             {publishedAt && (
               <div className="flex flex-col gap-1">
-                <p className="text-sm">Date Published</p>
+                <p className="text-sm">
+                  {post.language === 'es' ? 'Fecha de publicación' : 'Date Published'}
+                </p>
 
                 <time dateTime={publishedAt}>{formatDateTime(publishedAt)}</time>
               </div>
